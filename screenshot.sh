ebb5a77f069487c880d2a4e9b593883e50c49293
Sridhar D Kedlaya <sridhardked@gmail.com>
2023-05-05 12:30:53 +0530

    Add condition exiting without taking screenshot

--- screenshot.sh
+++ screenshot.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 
+set -e
 export XDG_SCREENSHOTS_DIR="/home/parzival/Screenshots/"
 a=$(echo "area\nactive\nscreen" | wofi -p "Select type of screenshot to be taken" -d)
-sleep 1
-grimblast save $a
+if [[ $a == "" ]]; then
+    exit
+else
+    sleep 1
+    grimblast save $a
+fi
