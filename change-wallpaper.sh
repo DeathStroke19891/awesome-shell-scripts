e1a7a234f4755f4e1a90907d7aeee49fa175c437
Sridhar D Kedlaya <sridhardked@gmail.com>
2023-11-24 23:26:00 +0530

    Reload wallpaper automatically every 5min

--- change-wallpaper.sh
+++ change-wallpaper.sh
@@ -2,17 +2,21 @@
 
 set -e 
 
-
 file=$1
 
-if [[ $file == "" ]]
-    then
-    files=(~/Pictures/Wallpapers/*)
-    randomfile=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
+files=(~/Pictures/Wallpapers/*)
 
-    wal -i $randomfile
-    swww img $randomfile --resize fit
-    else
+if [[ $file == "" ]]
+then
+    while [ true ]
+    do
+        random=$(awk 'BEGIN { srand(); print int(1 + rand() * 100)}')
+        randomfile=$(printf "%s\n" "${files[$random % ${#files[@]}]}")
+        wal -i $randomfile
+        swww img $randomfile --resize fit
+        sleep 300
+    done
+else
     wal -i $file
     swww img $file --resize fit 1> /dev/null
 fi
